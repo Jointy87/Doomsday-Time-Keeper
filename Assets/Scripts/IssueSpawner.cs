3af91c751dfd184dbf4d160a5f8434e8b34e1a63
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-05-02 17:24:01 +0200

    Initial commit

---
+++ Assets/Scripts/IssueSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IssueSpawner : MonoBehaviour
+{
+	//Config
+	[SerializeField] Transform[] issueSlots;
+
+	void Start()
+	{
+		
+	}
+
+
+	void Update()
+	{
+		
+	}
+}
