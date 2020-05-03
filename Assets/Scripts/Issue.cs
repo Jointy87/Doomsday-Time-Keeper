9e7386d43b2647bb954b2a6d0022bd35039eced0
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-05-03 17:37:50 +0200

    ADD: Added timer image to issue ADD: Added time decreasing by means of radial emptying

--- Assets/Scripts/Issue.cs
+++ Assets/Scripts/Issue.cs
@@ -7,10 +7,11 @@ public class Issue : MonoBehaviour
 {
 	//Config
 
-	[Header("Time per difficulty")]
+	[Header("Time")]
 	[Tooltip("Time in s to complete easy issue")] [SerializeField] float easyTimeAmount;
 	[Tooltip("Time in s to complete medium issue")] [SerializeField] float mediumTimeAmount;
 	[Tooltip("Time in s to complete hard issue")] [SerializeField] float hardTimeAmount;
+	[Tooltip("Timer visualization prefab")] [SerializeField] GameObject timerPrefab;
 	
 	[Header("Element slots")]
 	[Tooltip("The slots that will become the categories")] [SerializeField] GameObject[] issueCategorySlots;
@@ -19,16 +20,29 @@ public class Issue : MonoBehaviour
 	//Cache
 	int difficulty;
 	GlobalIssuesCategories gic;
+	float[] categoryTimes;
+	float issueTimeAmount;
+	Image timerImage;
+	float timeLeft;
 
 	void Awake()
 	{
+		categoryTimes = new float[] { easyTimeAmount, mediumTimeAmount, hardTimeAmount };
 		gic = FindObjectOfType<GlobalIssuesCategories>();
+		timerImage = timerPrefab.GetComponent<Image>();
+
 	}
 	void Start()
 	{
 		difficulty = Random.Range(1, 4);
 
 		SpawnCategories();
+		SetTimer();
+	}
+
+	void Update()
+	{
+		TimerCountDown();
 	}
 
 	private void SpawnCategories()
@@ -38,8 +52,7 @@ public class Issue : MonoBehaviour
 			var thisIssue = issueCategorySlots[diffRating];
 
 			SelectCategory(thisIssue);
-
-			thisIssue.gameObject.SetActive(true);
+			SpawnCategory(thisIssue);
 		}
 	}
 
@@ -48,5 +61,24 @@ public class Issue : MonoBehaviour
 		var issueCategory = gic.FetchGlobalIssueCategory(Random.Range(0, 6));
 		Sprite issueImage = gic.FetchGlobalIssueImage(issueCategory);
 		thisIssue.GetComponent<Image>().sprite = issueImage;
+	}
+	private void SpawnCategory(GameObject thisIssue)
+	{
+		thisIssue.gameObject.SetActive(true);
+	}
+
+	private void SetTimer()
+	{
+		issueTimeAmount = categoryTimes[difficulty-1];
+		timeLeft = issueTimeAmount;
+	}
+
+	private void TimerCountDown()
+	{
+		if(timeLeft > 0)
+		{
+			timeLeft -= Time.deltaTime;
+			timerImage.fillAmount = timeLeft / issueTimeAmount;
+		}
 	}
 }
