8304795e5ea0bad4e0599ed1042599884c609549
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-05-03 11:08:56 +0200

    ADD: created script

---
+++ Assets/Scripts/GlobalIssuesCategories.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalIssuesCategories : MonoBehaviour
+{
+	//Config
+	[SerializeField] Image climateIcon;
+	[SerializeField] Image pandemicIcon;
+	[SerializeField] Image etSecurityIcon;
+	[SerializeField] Image nuclearThreatIcon;
+	[SerializeField] Image globalSecurityIcon;
+	[SerializeField] Image aiIcon;
+
+	public enum GlobalIssues { climate, pandemic, etSecurity, nuclearThreat, globalSecurity, ai}
+
+	Image issueImage;
+	GlobalIssues chosenIssueCategory;
+
+	public GlobalIssues FetchGlobalIssueCategory(int issue)
+	{
+		chosenIssueCategory = (GlobalIssues)issue;
+		return chosenIssueCategory;
+	}
+	public Image FetchGlobalIssueImage(GlobalIssues issue)
+	{
+		if (issue == GlobalIssues.climate)
+		{
+			issueImage = climateIcon;
+		}
+		else if (issue == GlobalIssues.pandemic)
+		{
+			issueImage = pandemicIcon;
+		}
+		else if (issue == GlobalIssues.etSecurity)
+		{
+			issueImage = etSecurityIcon;
+		}
+		else if (issue == GlobalIssues.nuclearThreat)
+		{
+			issueImage = nuclearThreatIcon;
+		}
+		else if (issue == GlobalIssues.globalSecurity)
+		{
+			issueImage = globalSecurityIcon;
+		}
+		else if (issue == GlobalIssues.ai)
+		{
+			Image issueImage = aiIcon;
+		}
+
+		return issueImage;
+	}
+}
