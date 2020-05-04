c4e637fb3e5e46c3415094a837251e95383e6cda
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-05-04 12:40:38 +0200

    EDIT: Renamed expertdatacreator to expertdata EDIT: Started on expert expertise spawning. Ran into error when referencing SO referenced inside another SO. This commit contains that error

---
+++ Assets/Scripts/Data Creators/ExpertData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "ExpertDataFile")]
+public class ExpertData : ScriptableObject
+{
+	public string expertName;
+	public Image expertImage;
+	public int wakeTime;
+	public int sleepTime;
+	public ScriptableObject[] expertise;
+}
---
+++ Assets/Scripts/Expert.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Expert : MonoBehaviour
+{
+	//Config
+	[Header("Expert Data")]
+	[Tooltip("Expert data file")] [SerializeField] ExpertData expertDataFile;
+
+	[Header("Element Slots")]
+	[Tooltip("The slots that will become the expertise of expert")] [SerializeField] GameObject[] expertiseSlots;
+	[Tooltip("Image of the expert")] [SerializeField] GameObject expertImage;
+	
+	void Start()
+	{
+		SpawnExpertise();
+	}
+
+
+	void Update()
+	{
+		
+	}
+
+	private void SpawnExpertise()
+	{
+		var expertiseAmountToSpawn = expertDataFile.expertise.Length;
+
+		for(int expertiseAmount = 0; expertiseAmount < expertiseAmountToSpawn; expertiseAmount++)
+		{
+			var thisExpertise = expertiseSlots[expertiseAmount];
+
+			IssueCategoryData relevantExpertise = expertDataFile.expertise[expertiseAmount];	//THIS IS WHERE THE PROBLEM IS
+			Sprite expertiseImage = relevantExpertise.categoryImage.sprite;			
+		}
+	}
+}
