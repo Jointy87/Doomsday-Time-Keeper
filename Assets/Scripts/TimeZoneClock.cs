b99d6e9ae117850f15c508b38263547acb30710b
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-05-04 14:06:48 +0200

    ADD: Created clocks for experts and made it turn according to passed game time

---
+++ Assets/Scripts/TimeZoneClock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeZoneClock : MonoBehaviour
+{
+	//Config
+	[SerializeField] float realSecPerDay = 30f;
+	[SerializeField] Image clockHand;
+
+	//Cache
+	float day;
+	void Update()
+	{
+		day += Time.deltaTime / realSecPerDay;
+		float normalizedDay = day % 1f;
+		float rotationDegreesPerDay = 360f;
+		clockHand.transform.eulerAngles = new Vector3(0, 0, -normalizedDay * rotationDegreesPerDay);
+	}
+}
